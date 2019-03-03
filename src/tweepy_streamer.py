72ab58fe38ae1adec22d5b7ca04e3ff357ee9536
Jaime <jaimeliew1@gmail.com>
2019-03-03 16:00:05 +0100

    add streamer in twittermoto module

--- src/tweepy_streamer.py
+++ src/tweepy_streamer.py
@@ -1,34 +1,7 @@
 import tweepy
-from twittermoto import config
-
-class StreamListener(tweepy.StreamListener):
-
-    def on_connect(self):
-        """Called once connected to streaming server.
-        This will be invoked once a successful response
-        is received from the server. Allows the listener
-        to perform some work prior to entering the read loop.
-        """
-        print('Streamer connected...')
-        status_json = self.api.rate_limit_status()
-        limit = status_json['resources']['application']['/application/rate_limit_status']['limit']
-        remain = status_json['resources']['application']['/application/rate_limit_status']['remaining']
-        print(f'Resources: {remain}/{limit}')
+from twittermoto import config, StreamListener
 
 
-    def on_status(self, status):
-        """Called when a new status arrives"""
-        #print(status)
-        out = f'''@{status.user.screen_name}\n {status.text} \n \
-geo data: {status.geo}\n time: {status.created_at}\n'''
-        print(out)
-
-
-
-    def on_error(self, status_code):
-        """Called when a non-200 status code is returned"""
-        if status_code == 420:
-            return False
 
 
 if __name__ == '__main__':
--- twittermoto/twittermoto.py
+++ twittermoto/twittermoto.py
@@ -7,7 +7,7 @@ def run():
     if config.access_token is None:
         print('You haven''t set your Twitter access tokens yet. Please store your access tokens in config.py')
         return
-        
+
     auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
     auth.set_access_token(config.access_token, config.access_token_secret)
 
@@ -20,7 +20,7 @@ def run():
     lastID = api.search("earthquake", rpp=1).pop().id
     LMA, SMA = 1, 0
     hist = [9]*240
-    
+
     while True:
         startTime = time.time()
         count, geo_count = 0, 0
@@ -28,10 +28,10 @@ def run():
         #Check how many searches you have remaining (usually you get 180 per 15 minutes)
         searchesRemaining = api.rate_limit_status()['resources']['search']['/search/tweets']['remaining']
         print('Searches Remaining: ', searchesRemaining)
-        
+
         # Get tweets since last time we checked.
         tweets = tweepy.Cursor(api.search, q="earthquake OR terremoto OR tembor OR 地震 OR \"gempa bumi\" OR lindol OR lindu",since_id=lastID,rpp=100).items(200)
-        
+
         for i, tweet in enumerate(tweets):
             if SMA/LMA > 10:
                 print(time.strftime("%H:%M:%S: ", time.localtime(startTime)), tweet.text)
@@ -46,7 +46,41 @@ def run():
         SMA = sum(hist[0:4])
 
         print(f'{count} tweets about earthquakes, {geo_count} with geotags. Ratio = {round(SMA/LMA,2)}')
-        
+
         if time.time()-startTime < 15:
             time.sleep(15 - (time.time()-startTime))
-        
+
+
+
+class StreamListener(tweepy.StreamListener):
+
+    def on_connect(self):
+        """Called once connected to streaming server.
+        This will be invoked once a successful response
+        is received from the server. Allows the listener
+        to perform some work prior to entering the read loop.
+        """
+        print('Streamer connected...')
+        status_json = self.api.rate_limit_status()
+        limit = status_json['resources']['application']['/application/rate_limit_status']['limit']
+        remain = status_json['resources']['application']['/application/rate_limit_status']['remaining']
+        print(f'Resources: {remain}/{limit}')
+
+
+
+    def on_status(self, status):
+        '''EDIT THIS FUNCTION TO DO SOMETHING WHEN YOU RECIEVE A TWEET!!!'''
+        """Called when a new status arrives"""
+        #print(status)
+        out = f'''@{status.user.screen_name}\n {status.text} \n \
+geo data: {status.geo}\n time: {status.created_at}\n'''
+        print(out)
+        #print(status)
+        print()
+
+
+
+    def on_error(self, status_code):
+        """Called when a non-200 status code is returned"""
+        if status_code == 420:
+            return False
